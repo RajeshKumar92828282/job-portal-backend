59ea5ccfb767ab07da3b70be51a679f3bb35e161
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-04-13 16:46:28 +0530

    update

--- models/Application.js
+++ models/Application.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const mongoose= require("mongoose");
 
 const applicationSchema= new mongoose.Schema({
@@ -29,3 +30,6 @@ coverleter:{
 },
 {timestamps: true}
 );
+
+applicationSchema.index({job:1,application:1},{unique:true});
+module.exports= mongoose.model("Application",applicationSchema);
